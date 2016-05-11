dc108b06aefe2333cac914aaabaefa617b685445
avidms <avram.dames@gmail.com>
2016-05-11 18:47:15 +0200

    change the insert method to batch

--- kumpel/__init__.py
+++ kumpel/__init__.py
@@ -1,9 +1,9 @@
 from kumpel.helpers import read_text_file
 
 # Database Modules
-from kumpel.postgresql import Table
+from kumpel.postgresql import Table, Database
 from kumpel.client import Source, Query
 
 
-__all__ = [Source, Query, Table, read_text_file]
+__all__ = [Source, Query, Table, Database, read_text_file]
 __doc__ = """ Kumpel """
