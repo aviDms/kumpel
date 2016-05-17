f010aacdc92f09ec209a5767a79164471dd6c5d6
avidms <avram.dames@gmail.com>
2016-05-17 10:57:58 +0200

    add ga and read records for table

---
+++ kumpel/google_spreadsheets.py
@@ -0,0 +1,35 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+
+class GoogleSpreadsheets(object):
+    """ """
+    def __init__(self, credentials_file):
+        self.scopes = ['https://spreadsheets.google.com/feeds']
+        self.credentials_file = credentials_file
+
+    def add_scope(self):
+        """
+
+        :return:
+        """
+        raise NotImplementedError
+
+    def get_records(self, workbook, worksheet, empty2zero=False, head=1):
+        """
+
+        :param workbook:
+        :param worksheet:
+        :param empty2zero:
+        :param head:
+        :return:
+        """
+        credentials = ServiceAccountCredentials.from_json_keyfile_name(
+            filename=self.credentials_file,
+            scopes=self.scopes
+        )
+        drive = gspread.authorize(credentials)
+        doc = drive.open(title=workbook)
+        sheet = doc.worksheet(title=worksheet)
+        for record in sheet.get_all_records(empty2zero=empty2zero, head=head):
+            yield record
